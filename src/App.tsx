ae763b53a3a6048de3bae9035b74ff631d7c67ae
ehsemloh <ehsemloh@gmail.com>
2025-05-28 18:57:31 +1200

    add global error boundary to app level

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import { NavigationContainer } from "@react-navigation/native";
 import FeedListScreen from "./FeedListScreen";
 import FeedScreen from "./FeedScreen";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ErrorBoundary from "./ErrorBoundary";
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={FeedListScreen} />
-        <Stack.Screen name="Feed" component={FeedScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ErrorBoundary fallbackView={null}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={FeedListScreen} />
+          <Stack.Screen name="Feed" component={FeedScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ErrorBoundary>
   );
 }
 
